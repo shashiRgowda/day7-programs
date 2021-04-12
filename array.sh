f0616973d29618df020f72d8e115432b7a0d913f
shashiRgowda <shashikumartr64@gmail.com>
2021-04-12 16:28:14 +0530

    Added:addinteger.sh

---
+++ array.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+n=0
+for((  number=1; number<=100; number++ ))
+do
+ if [ $number -ge 11 ]
+   if [ $number -gt 1 ]
+   then
+   n=$number
+   sum=0
+   reminder=0
+
+  while [ $n -gt 0 ]
+  do
+
+    reminder=$(($number%10))
+    sum=$((($sum*10)+$reminder))
+    n=$(($n/10))
+fi 
+fi
+   done
+
+  if [ $(($number)) -eq $sum ]
+  then
+      echo $number
+  fi
+
+
+
+
+
+
